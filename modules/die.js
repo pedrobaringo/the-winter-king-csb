09f980d98c7299a4114d4c217f249f57d9c9f1bd
pedrobaringo <pedro.baringo@gmail.com>
2024-02-28 14:16:58 +0100

    1.0.1

--- modules/die.js
+++ modules/die.js
@@ -17,4 +17,30 @@ export class RIWhite extends Die {
             "6": '<img src="modules/the-winter-king-csb/assets/Dice/Dice6_inCHAT.webp" />'
         }[result.result];
     }
-}
+};
+export class RId6 extends Die {
+    constructor(termData) {
+        super(termData);
+    }
+
+/** @override */
+    static DENOMINATION = "d";
+     /** @override */
+    
+    getResultLabel(result) {
+     if(this.faces==6) {
+        return {
+            "1": '<img src="modules/the-winter-king-csb/assets/Dice/Dice1_inCHAT.webp" />',
+            "2": '<img src="modules/the-winter-king-csb/assets/Dice/Dice2_inCHAT.webp" />',
+            "3": '<img src="modules/the-winter-king-csb/assets/Dice/Dice3_inCHAT.webp" />',
+            "4": '<img src="modules/the-winter-king-csb/assets/Dice/Dice4_inCHAT.webp" />',
+			"5": '<img src="modules/the-winter-king-csb/assets/Dice/Dice5_inCHAT.webp" />',			
+            "6": '<img src="modules/the-winter-king-csb/assets/Dice/Dice6_inCHAT.webp" />'
+        }[result.result];
+        } else {
+        return String(result.result);
+        }
+    }
+    
+    
+};
--- modules/winterking.js
+++ modules/winterking.js
@@ -1,13 +1,36 @@
 import {RIWhite} from './die.js';
+import {RId6} from './die.js';
 
 Hooks.once("init", async function () {
     CONFIG.Dice.terms["i"] = RIWhite;
+	CONFIG.Dice.terms["d"] = RId6;
 });
 
 Hooks.once('diceSoNiceReady', (dice3d) => {
     dice3d.addSystem({id:"RIDice",name:"Winter King Dice"},"preferred");
     dice3d.addDicePreset({
       type:"di",
+      labels:[
+        'modules/the-winter-king-csb/assets/Dice/Dice1.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice2.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice3.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice4.webp',
+        'modules/the-winter-king-csb/assets/Dice/Dice5.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice6.webp'
+      ],
+      bumpMaps:[
+        'modules/the-winter-king-csb/assets/Dice/Dice1_Bump.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice2_Bump.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice3_Bump.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice4_Bump.webp',
+        'modules/the-winter-king-csb/assets/Dice/Dice5_Bump.webp', 
+        'modules/the-winter-king-csb/assets/Dice/Dice6_Bump.webp'
+      ],
+	  colorset:"whiteRI",
+      system: "RIDice"
+    });
+	dice3d.addDicePreset({
+      type:"d6",
       labels:[
         'modules/the-winter-king-csb/assets/Dice/Dice1.webp', 
         'modules/the-winter-king-csb/assets/Dice/Dice2.webp', 
